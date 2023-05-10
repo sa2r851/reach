1e7a38abdb4b1fea1927eb97903caae350f50a03
Mohamed sakr <62855466+sa2r851@users.noreply.github.com>
2023-05-10 20:19:51 +0300

    Delete project directory

--- project/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import api
-app_name='account'
-urlpatterns = [
-    #create account
-    path('register-ph/', api.PharmacyRegistration.as_view(), name='Register-Pharmacy'),
-    path('register-customer/', api.CustomerRegistration.as_view(), name='Register-Customer'),
-
-]
